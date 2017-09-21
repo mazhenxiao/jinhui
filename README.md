bd599c09e77eacd0873c5de4862f6abae8e88bc0
mazhenxiao@gmail.com <mazhenxiao@gmail.com>
2017-09-21 18:16:48 +0800

    提交

---
+++ README.md
@@ -0,0 +1,44 @@
+# jinhui
+金辉门户
+============
+* dataguid数据绑定格式
+1. page=当前页数
+2. rows=条数
+3. GET 拉取服务端数据http://www.jeasyui.net/demo/datagrid_data1.json?page=2&rows=10
+~~~ json
+{
+total: 20,  //总条目
+rows: [
+{ type: "field对应列字段", content: "field对应列字段", people: "field对应列字段", time:"field对应列字段" }
+]
+}
+~~~
+* 动态表格生成
+
+| 名称 | 值 | 说明 |
+| ------------- |:-------------:| -----:|
+| id | 控件id | 控件唯一id用来对应服务器字段 | 
+| key | 显示名称 | 比如select的txt值 | 
+| val | 值 | 比如select value值 |
+|type| 类型|input 或 select|
+| data | ｛key：”“，val：”“｝ | 如果select data数据key值为txt值 val值为实际值  |
+
+~~~ json
+[{
+    id:"",//控件id
+    key:"key",  //显示文本值
+    val:"",    //提交value值
+    type:"input",  //类型
+    data:[]
+},
+{
+    id:"",
+   key:"key",
+    val:"",
+    type:"select",
+    data:[
+        {key:"key",val:""}
+    ]
+}
+]
+~~~
--- components/component-agenty.js
+++ components/component-agenty.js
@@ -11,47 +11,47 @@ class Agenty extends React.Component {
         this.agentyTabel();//绑定表哥 
     }
     agentyTabel() {
-        let opens=(arg,data)=>{
+        let opens = (arg, data) => {
             console.log(arg);
         }
-        let formatter=(txt,data)=>{
+        let formatter = (txt, data) => {
             return `<a href='javascript:;' >${txt}</a>`
         }
         let tabel = $("#agentyBoxTabel"),
             col = [[
-                {field:"type",title:"审批类型",align:"center" ,width: 120, fixed: true,formatter:formatter},
-                {field:"content",title:"审批内容",align:"center",width:120},
-                {field:"people",title:"提交人",align:"center", width: 120, fixed: true},
-                {field:"time",title:"提交时间",align:"center", width: 120, fixed: true}
+                { field: "type", title: "审批类型", align: "center", width: 120, fixed: true, formatter: formatter },
+                { field: "content", title: "审批内容", align: "center", width: 120 },
+                { field: "people", title: "提交人", align: "center", width: 120, fixed: true },
+                { field: "time", title: "提交时间", align: "center", width: 120, fixed: true }
             ]],
-            da={
-                total:20,
-                rows:[
-                {type:"审批类型",content:"1审批内容",people:"提交人",time:"2017-20-20"},
-                {type:"审批类型",content:"2审批内容",people:"提交人",time:"2017-20-20"},
-                {type:"审批类型",content:"3审批内容",people:"提交人",time:"2017-20-20"},
-                {type:"审批类型",content:"4审批内容",people:"提交人",time:"2017-20-20"},
-                {type:"审批类型",content:"5审批内容",people:"提交人",time:"2017-20-20"},
-                {type:"审批类型",content:"6审批内容",people:"提交人",time:"2017-20-20"},
-                {type:"审批类型",content:"7审批内容",people:"提交人",time:"2017-20-20"},
-                {type:"审批类型",content:"8审批内容",people:"提交人",time:"2017-20-20"},
-                {type:"审批类型",content:"9审批内容",people:"提交人",time:"2017-20-20"},
-                {type:"审批类型",content:"0审批内容",people:"提交人",time:"2017-20-20"}
-            ]
-        };
+            da = {
+                total: 20,
+                rows: [
+                    { type: "审批类型", content: "1审批内容", people: "提交人", time: "2017-20-20" },
+                    { type: "审批类型", content: "2审批内容", people: "提交人", time: "2017-20-20" },
+                    { type: "审批类型", content: "3审批内容", people: "提交人", time: "2017-20-20" },
+                    { type: "审批类型", content: "4审批内容", people: "提交人", time: "2017-20-20" },
+                    { type: "审批类型", content: "5审批内容", people: "提交人", time: "2017-20-20" },
+                    { type: "审批类型", content: "6审批内容", people: "提交人", time: "2017-20-20" },
+                    { type: "审批类型", content: "7审批内容", people: "提交人", time: "2017-20-20" },
+                    { type: "审批类型", content: "8审批内容", people: "提交人", time: "2017-20-20" },
+                    { type: "审批类型", content: "9审批内容", people: "提交人", time: "2017-20-20" },
+                    { type: "审批类型", content: "0审批内容", people: "提交人", time: "2017-20-20" }
+                ]
+            };
         tabel.datagrid({
             width: "auto",
             nowrap: true,
             fitColumns: true,
             rownumbers: true,
             singleSelect: true,
-            striped:true,
-            pagination:true,
+            striped: true,
+            pagination: true,
             columns: col,
-            data:da
-         
+            data: da
+
         });
-      
+
     }
     render() {
         return <article>
