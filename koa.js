d6012fc269b9b0602ff1fa9a3828a1412b3de48b
ZhangRuiTao <381596533@qq.com>
2017-11-30 19:43:35 +0800

    注释启动浏览器

--- koa.js
+++ koa.js
@@ -28,20 +28,20 @@ app.use(async (ctx, next) => {
 router.get('/', async (ctx, next) => {
     await ctx.render("login", {layout: false})
 })
-.get('/login', async (ctx, next) => {
-    await ctx.render("login", {layout: false})
-})
-.get('/index', async (ctx, next) => {
-    // this // ctx.render("index",{layout:false})
-    await ctx.render("index", {layout: false})
-})
-.get('/AreaInfo', async (ctx, next) => {
-    await ctx.render("AreaInfo", {layout: false})
-})
+    .get('/login', async (ctx, next) => {
+        await ctx.render("login", {layout: false})
+    })
+    .get('/index', async (ctx, next) => {
+        // this // ctx.render("index",{layout:false})
+        await ctx.render("index", {layout: false})
+    })
+    .get('/AreaInfo', async (ctx, next) => {
+        await ctx.render("AreaInfo", {layout: false})
+    });
 
 app.listen(3000, arg => {
     // c.exec("npm run dev");
     console.log("启动成功，请访问 http://localhost:3000/login");
 });
 
-c.exec('start http://localhost:3000/login');
+//c.exec('start http://localhost:3000/login');
--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
-    publicPath:"./pocs-web",
+    publicPath: "./pocs-web",
     hot: true,
     historyApiFallback: false
 }).listen(5001
-    ,'localhost', function (err, result) {
-    var c = require('child_process');
-        c.exec('start http://localhost:5001/index/#/');
+    , 'localhost', function (err, result) {
+        // var c = require('child_process');
+        //     c.exec('start http://localhost:5001/index/#/');
         if (err) {
             console.log(err);
         }
-       // console.log('Listening at localhost:5000');
+        // console.log('Listening at localhost:5000');
     });
