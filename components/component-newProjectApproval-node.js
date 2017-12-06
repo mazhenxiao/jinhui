01beb01bae8cb58bfc34c9cfb44399edfddb94f1
mazhenxiao <mazhenxiao@hotmail.com>
2017-12-06 20:33:14 +0800

    提交驳回、通过、审批

--- components/component-newProjectApproval-node.js
+++ components/component-newProjectApproval-node.js
@@ -7,7 +7,6 @@
 import React from 'react';
 import "../js/iss.js";
 import "babel-polyfill";  //兼容ie
-
 import "../css/newProjectApproval.less";
 class ApprovalControlNode extends React.Component {
     constructor(arg) {
@@ -16,34 +15,44 @@ class ApprovalControlNode extends React.Component {
             aOpinions: "审核意见",
             aList: [],
             InfoData: [],//流程信息
-            allSearchArg:this.props.allSearchArg,
-            history:[] //历史纪录
+            allSearchArg: this.props.allSearchArg,
+            history: [] //历史纪录
         }
-        this.type ="edit"  //this.props["type"] || "edit"; //以防外部没有设置type类型
-        
+        this.type = "edit"  //this.props["type"] || "edit"; //以防外部没有设置type类型
+
         this.selectedFlows = [] //选人数据 
-        this.currentApprovalList=sessionStorage.getItem("currentApprovalList")? JSON.parse(sessionStorage.getItem("currentApprovalList")):[];
+        this.currentApprovalList = sessionStorage.getItem("currentApprovalList") ? JSON.parse(sessionStorage.getItem("currentApprovalList")) : [];
     }
     componentWillMount() {
+        let { e, dataKey } = this.state.allSearchArg,th = this;
         if (this.props.callback) {
             this.props.callback(this);
         }
-        this.GetAjax();
+        if (e == "10102") {
+            this.EVENT_CLICK_SUBMIT()
+            .then(arg=>{
+                
+                th.GetAjax(arg);
+            })
+        } else {
+            this.GetAjax();
+        }
+
 
     }
     /*监听审核意见*/
     changeAOinions(event) {
         this.setState({ aOpinions: event.target.value });
     }
-    GetAjax() {
+    GetAjax(arg) {
         let th = this;
-        let allSearchArg=th.state.allSearchArg;
+        let allSearchArg = th.state.allSearchArg;
         let getInfo = {
             entiId: allSearchArg['e'],
-            dataKey: allSearchArg['dataKey'],//==================================================
+            dataKey:arg||allSearchArg['dataKey'],//==================================================
             userId: iss.userInfo.ID,
-            comanyId:allSearchArg["areaId"],
-            comanyName:allSearchArg["areaName"]
+            comanyId: allSearchArg["areaId"],
+            comanyName: allSearchArg["areaName"]
         }
         iss.ajax({ //流程导航
             url: "/iWorkflow/Workflow/api/WFServices.asmx/GetSubmitWorkflows",
@@ -52,7 +61,7 @@ class ApprovalControlNode extends React.Component {
             contentType: "application/json;charset=utf-8",
             data: JSON.stringify(getInfo),
             success(result) {
-            	
+
                 th.setState({
                     InfoData: JSON.parse(result.d.Data)
                 });
@@ -77,15 +86,15 @@ class ApprovalControlNode extends React.Component {
         });
     }
     EVENT_CHANGE_LIST(da, ev) { //修改
-        var th = this,_data = this.state.InfoData[0]["Flows"];
+        var th = this, _data = this.state.InfoData[0]["Flows"];
         var id = (ev.target.value);
-        for(var i = 0 ;i< this.selectedFlows.length;i++){
-            if(this.selectedFlows[i]["ContextGuid"]==da.Id){
-                this.selectedFlows[i]["Participants"]=id;
-                
+        for (var i = 0; i < this.selectedFlows.length; i++) {
+            if (this.selectedFlows[i]["ContextGuid"] == da.Id) {
+                this.selectedFlows[i]["Participants"] = id;
+
             }
         }
-        sessionStorage.setItem("currentApprovalList",JSON.stringify(this.selectedFlows));
+        sessionStorage.setItem("currentApprovalList", JSON.stringify(this.selectedFlows));
     }
     EVENT_CHANGE_CHECKBOX(da, ev) {//input
         var ta = ev.target;
@@ -109,56 +118,57 @@ class ApprovalControlNode extends React.Component {
                 return;
             }
         });
-        sessionStorage.setItem("currentApprovalList",JSON.stringify(this.selectedFlows));
+        sessionStorage.setItem("currentApprovalList", JSON.stringify(this.selectedFlows));
     }
     //========提交、返回=========================================
-    evConfirmSubmitTo(){
-        var th=this;
-        let {e,dataKey}=th.state.allSearchArg
-       // iss.checkLogin(arg=>{  //暂时注销
-            if(e=="10102"){  //只有项目提交dataKey变更为返回值
-                iss.evConfirmAlert("是否确认提交",th.EVENT_CLICK_SUBMIT.bind(th));
-            }else{
-                iss.evConfirmAlert("是否确认提交",th.BIND_CHECKED.bind(th));
-            }
-       // })
-    	
+    evConfirmSubmitTo() {
+        var th = this;
+        let { e, dataKey } = th.state.allSearchArg
+        // iss.checkLogin(arg=>{  //暂时注销
+
+        iss.evConfirmAlert("是否确认提交", th.BIND_CHECKED.bind(th));
+
+        // })
+
     }
     EVENT_CLICK_SUBMIT() {  //当前填报人提交
         var th = this;
-        let {e,dataKey}=th.state.allSearchArg
-        iss.ajax({ //老代码不再进行封装修改。
-            url:"/Stage/ICreateProVersion",
-            data:{
-                "id":dataKey  //==================================================
-            },
-            success(da){
-                if(da["rows"]){
-                    th.BIND_CHECKED(da["rows"])
-                }else{
-                    iss.error("获取新版本id失败")
-                }
-               
-            },
-            error(){
+        let { e, dataKey } = th.state.allSearchArg
+      return  new Promise((resolve, reject) => {
+            iss.ajax({ //老代码不再进行封装修改。
+                url: "/Stage/ICreateProVersion",
+                data: {
+                    "id": dataKey  //==================================================
+                },
+                success(da) {
+                    if (da["rows"]) {
+                        resolve(da["rows"]);
+                        //th.BIND_CHECKED(da["rows"])
+                    } else {
+                        iss.error("获取新版本id失败")
+                    }
 
-            }
+                },
+                error() {
+
+                }
+            })
         })
-      //  th.BIND_CHECKED();  //检查数据
+        //  th.BIND_CHECKED();  //检查数据
     }
     BIND_CHECKED(newId) {   //第一次ajax提交检查数据
         debugger
-    	var th = this;
-    	var allSearchArg=th.state.allSearchArg;
-    	
+        var th = this;
+        var allSearchArg = th.state.allSearchArg;
+
         var dto = {
             "runtimeUnique": {
-                EntiId:allSearchArg['e'],// 实体ID
-                DataKey:newId||allSearchArg['dataKey']// 业务ID======================================
+                EntiId: allSearchArg['e'],// 实体ID
+                DataKey: newId || allSearchArg['dataKey']// 业务ID======================================
             }
         };
         var turnOut = true;
-        
+
         iss.ajax({
             url: "/iWorkflow/Workflow/api/WFServices.asmx/IsSubmitted",
             type: "POST",
@@ -168,8 +178,8 @@ class ApprovalControlNode extends React.Component {
             success: function (result) {
                 if (result.d["Data"] == "false" && result.d["Success"] == true) {
                     th.BIND_CHECKEDSUCESS(newId);//二次提交
-                }else{
-                    iss.popover({content:result.d.Message});
+                } else {
+                    iss.popover({ content: result.d.Message });
                     $(window).trigger("treeLoad");
                 }
             }
@@ -178,20 +188,20 @@ class ApprovalControlNode extends React.Component {
     }
     BIND_CHECKEDSUCESS(newId) {  //当前填报人第二次ajax提交提交流程
         let th = this;
-        var allSearchArg=th.state.allSearchArg;
-        let basicInfor= {
-            DataKey: newId||allSearchArg['dataKey'],// 业务ID,================================================
+        var allSearchArg = th.state.allSearchArg;
+        let basicInfor = {
+            DataKey: newId || allSearchArg['dataKey'],// 业务ID,================================================
             EntiId: allSearchArg["e"],
             EventUserId: iss.userInfo.ID,//当前登陆人
             Files: [],//附件
-            ProcessComment:this.props["data"]||""
+            ProcessComment: this.props["data"] || ""
         }
-      
-        let sbarr2 = this.selectedFlows.map((el,ind)=>{
-               if(typeof el.Participants=="string"){
-                    el.Participants=[el.Participants.toString()];
-               }
-               return el;
+
+        let sbarr2 = this.selectedFlows.map((el, ind) => {
+            if (typeof el.Participants == "string") {
+                el.Participants = [el.Participants.toString()];
+            }
+            return el;
         })
 
         let submitdata = JSON.stringify({
@@ -208,24 +218,24 @@ class ApprovalControlNode extends React.Component {
                 var rt = result.d;
                 // turnOut = rt.Success;
                 if (rt.Success == true) {
-                    iss.popover({content:"提交成功！",type:2});
+                    iss.popover({ content: "提交成功！", type: 2 });
                     sessionStorage.removeItem("currentApprovalText");//清楚临时历史数据存储，解决路由切换不能记录用户输入内容问题
                     sessionStorage.removeItem("currentApprovalList");//清楚临时历史数据存储，解决路由切换不能记录用户选择内容问题
                     $(window).trigger("treeLoad");
-                    iss.hashHistory.push({pathname:"agenty"});
-                   
+                    iss.hashHistory.push({ pathname: "agenty" });
+
                 } else {
-                    iss.popover({content:rt.Message});
+                    iss.popover({ content: rt.Message });
                 }
             }
         });
 
     }
-    setInfoDataList() {  
+    setInfoDataList() {
         var th = this;
         if (!th.state.InfoData.length) { return }
         let list = th.state.InfoData[0]["Flows"];
-        th.selectedFlows = th.currentApprovalList.length? th.currentApprovalList:[];
+        th.selectedFlows = th.currentApprovalList.length ? th.currentApprovalList : [];
         return list.map((el, ind) => {
             let submit =
                 { //提交数据
@@ -237,16 +247,16 @@ class ApprovalControlNode extends React.Component {
                     Participants: [], //用户
                     RunFlowId: "0"//流程节点
                 }, userArra = [];
-                !th.currentApprovalList.length&&th.selectedFlows.push(submit);  //按地址引用先push后修改
-            let _id=this.currentApprovalList.length? this.currentApprovalList[ind]["Participants"]:"";
+            !th.currentApprovalList.length && th.selectedFlows.push(submit);  //按地址引用先push后修改
+            let _id = this.currentApprovalList.length ? this.currentApprovalList[ind]["Participants"] : "";
             if (el.Type == "Approve" && th.type == "edit" && el.Users.length >= 2) {
-                let arr1 =  el.Users.filter((v1,i1)=>{ return v1});
+                let arr1 = el.Users.filter((v1, i1) => { return v1 });
                 return <li key={ind}>
                     <span>{el.Text}</span>
                     <select ref="select" defaultValue={_id} onChange={th.EVENT_CHANGE_LIST.bind(th, el)}>
                         {
                             arr1.map((ee, ii) => {
-                               
+
                                 if (ii == 0) {
                                     userArra.push(ee.UId);
                                     submit.Participants = userArra;
@@ -257,23 +267,23 @@ class ApprovalControlNode extends React.Component {
                     </select>
                 </li>
             } else if (el.Type == "AutoInform" && th.type == "edit") {
-                let arr2 =  el.Users.filter((v2,i2)=>{ return v2});
-                _id=(typeof _id!="string")? _id.join(","):"";
+                let arr2 = el.Users.filter((v2, i2) => { return v2 });
+                _id = (typeof _id != "string") ? _id.join(",") : "";
                 return <li key={ind} ><span>{el.Text}【
                     {
                         arr2.map((h, l) => {
-                            
+
                             userArra.push(h.UId);
                             submit.Participants = userArra;
-                            return <label key={l}><input key={l} type="checkbox" defaultChecked={_id? _d.indexOf(h.UId)>=0:"true" } value={h.UId} onChange={th.EVENT_CHANGE_CHECKBOX.bind(th, el)} />{h.Name + (l == el.Users.length - 1 ? "" : ",")}</label>
+                            return <label key={l}><input key={l} type="checkbox" defaultChecked={_id ? _d.indexOf(h.UId) >= 0 : "true"} value={h.UId} onChange={th.EVENT_CHANGE_CHECKBOX.bind(th, el)} />{h.Name + (l == el.Users.length - 1 ? "" : ",")}</label>
                         })
                     }
                     】</span>
                 </li>
             } else {
-                let arr3 =  el.Users.filter((v3,i3)=>{ return v3});
+                let arr3 = el.Users.filter((v3, i3) => { return v3 });
                 let str = arr3.map((vv, jj) => {
-                    
+
                     for (let i = 0; i < userArra.length; i++) {
                         //  if(userArra[i]["Id"])
                     }
@@ -291,38 +301,38 @@ class ApprovalControlNode extends React.Component {
         }, this);
     }
     //========通过、驳回===========================================
-    EVENT_CLICK_PASS(){
+    EVENT_CLICK_PASS() {
 
     }
-    Event_click_cancel(){ //取消
-        var url="";
-        switch(this.props.allSearchArg.e){
-            case iss.getEVal("intallmentStatus"):url="intallment";break; //分期
-            case iss.getEVal("newProjectStatus"):url="newProject";break;//项目
-            case iss.getEVal("teamMaintainStatus"):url="AreaInfo/groupbuild";break;//项目团队维护
+    Event_click_cancel() { //取消
+        var url = "";
+        switch (this.props.allSearchArg.e) {
+            case iss.getEVal("intallmentStatus"): url = "intallment"; break; //分期
+            case iss.getEVal("newProjectStatus"): url = "newProject"; break;//项目
+            case iss.getEVal("teamMaintainStatus"): url = "AreaInfo/groupbuild"; break;//项目团队维护
         }
         sessionStorage.removeItem("currentApprovalText");//清楚临时历史数据存储，解决路由切换不能记录用户输入内容问题
         sessionStorage.removeItem("currentApprovalList");//清楚临时历史数据存储，解决路由切换不能记录用户选择内容问题
-        if(this.props.allSearchArg["newId"]){
+        if (this.props.allSearchArg["newId"]) {
             iss.hashHistory.replace({
-                pathname:`/${url}`,
-                search:`?status=edit&dataKey=${this.props.allSearchArg.newId}&e=${this.props.allSearchArg.e}&isProOrStage=${this.props.allSearchArg.isProOrStage}`
+                pathname: `/${url}`,
+                search: `?status=edit&dataKey=${this.props.allSearchArg.newId}&e=${this.props.allSearchArg.e}&isProOrStage=${this.props.allSearchArg.isProOrStage}`
             });
-            location.href=`${location.origin}${location.pathname.replace(/\/$/ig,"")}/#/${url}?status=edit&dataKey=${this.props.allSearchArg.newId}&e=${this.props.allSearchArg.e}&isProOrStage=${this.props.allSearchArg.isProOrStage}`
-        }else{
+            location.href = `${location.origin}${location.pathname.replace(/\/$/ig, "")}/#/${url}?status=edit&dataKey=${this.props.allSearchArg.newId}&e=${this.props.allSearchArg.e}&isProOrStage=${this.props.allSearchArg.isProOrStage}`
+        } else {
             iss.hashHistory.replace({
-                pathname:`/${url}`,
-                search:`?status=edit&dataKey=${this.props.allSearchArg.dataKey}&e=${this.props.allSearchArg.e}`
+                pathname: `/${url}`,
+                search: `?status=edit&dataKey=${this.props.allSearchArg.dataKey}&e=${this.props.allSearchArg.e}`
             });
-            location.href=`${location.origin}${location.pathname.replace(/\/$/ig,"")}/#/${url}?status=edit&dataKey=${this.props.allSearchArg.dataKey}&e=${this.props.allSearchArg.e}`
+            location.href = `${location.origin}${location.pathname.replace(/\/$/ig, "")}/#/${url}?status=edit&dataKey=${this.props.allSearchArg.dataKey}&e=${this.props.allSearchArg.e}`
         }
-        
+
     }
     BIND_CHECKEDIT() {
         if (this.type != "edit") {
             return <p className="btnBox">
-                <a className="btn" href="javascript:;" onClick={this.EVENT_CLICK_PASS.bind(this)}>通过</a>
-                <a className="btn" href="javascript:;">驳回</a>
+                {/*    <a className="btn" href="javascript:;" onClick={this.EVENT_CLICK_PASS.bind(this)}>通过</a>
+                <a className="btn" href="javascript:;">驳回</a> */}
             </p>
         } else {
             return <p className="btnBox">
@@ -333,15 +343,15 @@ class ApprovalControlNode extends React.Component {
     }
     BIND_HISTORY() {
         return this.state.history.map((el, ind) => {
-             return <tr key={ind}>
-                 <td>{el.FLOWNAME}</td>
-                 <td>{el.PROCESSCOMMENT}</td>
-                 <td>{el.EVENTUSERNAME}</td>
-                 <td>{el.EVENTDATE}</td>
-                 <td>{el.TRACKINGEVENTTEXT}</td>
-             </tr>
-         })
-     }
+            return <tr key={ind}>
+                <td>{el.FLOWNAME}</td>
+                <td>{el.PROCESSCOMMENT}</td>
+                <td>{el.EVENTUSERNAME}</td>
+                <td>{el.EVENTDATE}</td>
+                <td>{el.TRACKINGEVENTTEXT}</td>
+            </tr>
+        })
+    }
     render() {
 
         var re_aOpinions = this.state.aOpinions;
@@ -369,9 +379,9 @@ class ApprovalControlNode extends React.Component {
 
                 </tbody></table>
             {
-              
-              this.BIND_CHECKEDIT()
-                
+
+                this.BIND_CHECKEDIT()
+
             }
 
             <table className="table tableProject approvalProcess">
@@ -382,9 +392,9 @@ class ApprovalControlNode extends React.Component {
                     <th>操作时间</th>
                     <th>操作</th>
                 </tr>
-                {
-                    this.BIND_HISTORY()
-                }
+                    {
+                        this.BIND_HISTORY()
+                    }
                 </tbody></table>
         </div>);
 
