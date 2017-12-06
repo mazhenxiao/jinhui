915ada9e4e0de2d11678fafe8d50d96ca8b79d24
张瑞涛 <381596533@qq.com>
2017-12-06 22:28:28 +0800

    修改 签约与回款

--- payment/sign-index.js
+++ payment/sign-index.js
@@ -1,7 +1,7 @@
 import "babel-polyfill";  //兼容ie
 import iss from "../js/iss.js";
 import React, {Component} from 'react';
-import {Table, Input, Select} from 'antd';
+import {Spin, Tabs, Row, Col, Button, Select} from 'antd';
 import {WrapperTreeTable} from '../common';
 import {AreaService} from '../services';
 
@@ -15,16 +15,66 @@ class Index extends Component {
 
     state = {
         loading: false,
+        dataKey: this.props.location.query.dataKey || "", /*项目id或分期版本id*/
+        mode: this.props.location.query.isProOrStage == "1" ? "Project" : "Stage",//显示模式，项目或者分期
     };
 
-    componentWillMount() {
+    componentDidMount() {
 
     }
 
+    /**
+     * 在组件接收到一个新的prop时被调用,这个方法在初始化render时不会被调用
+     * param nextProps 下一阶段的props
+     */
+    componentWillReceiveProps(nextProps) {
+        const {dataKey} = this.state;
+        const {location} = nextProps;
+        const nextDataKey = location.query.dataKey || "";
+        let nextMode = location.query.isProOrStage || "";
+        nextMode = nextMode == "1" ? "Project" : "Stage";
+
+        //切换路由之后，重新获取数据
+
+        if (dataKey != nextDataKey) {
+            this.setState({
+                    dataKey: nextDataKey,
+                    mode: nextMode,
+                    activeTapKey: "plan-quota",
+                }
+            );
+            this.loadStep(nextDataKey, nextMode);
+        }
+    }
+
+    getApprovalState = () => {
+        if (this.props.location.query["current"] == "ProcessApproval") {
+            return true;
+        }
+        return false;
+    };
+
+    /**
+     *  渲染空页面
+     */
+    renderEmpty = () => {
+        return (
+            <div className="processBar">
+                请点击左侧树，项目/分期
+            </div>
+        );
+    };
+
     render() {
+        const {dataKey} = this.state;
+        if (!dataKey) {
+            return this.renderEmpty();
+        }
 
         return (
-            <WrapperTreeTable></WrapperTreeTable>
+            <div>
+                <WrapperTreeTable></WrapperTreeTable>
+            </div>
         );
     }
 }
