297da14c0c5093316a0a404ccdd0cc6b9d0f8962
张瑞涛 <381596533@qq.com>
2017-12-11 22:01:04 +0800

    添加流程审批标题

--- components/component-ProcessApprover.js
+++ components/component-ProcessApprover.js
@@ -1,48 +1,72 @@
 /*审批信息*/
 import React from 'react';
 /**
- * 审批人   
- * 
+ * 审批人
+ *
  */
 import "../js/iss.js";
 import "babel-polyfill";  //兼容ie
 import ProcessApprovalTab from "./component-ProcessApproval-Tab.js"; //导航信息
 import ApprovalControlNode2 from "./component-newProjectApprover-node.js";  //审批信息
+import {CommonService} from '../services';
+
 require("../css/processApproval.less");
+
 class ProcessApproval extends React.Component {
     constructor(arg) {
         super(arg);
         this.state = {
-            remarkTxt:"", //备注内容
+            remarkTxt: "", //备注内容
             type: "readonly",  //edit自己readonly审批人
-            allSearchArg:this.props.location.query,/*地址栏所有参数*/
-            guid:this.props.location.query.dataKey  //页面传入表单guid    
+            allSearchArg: this.props.location.query, /*地址栏所有参数*/
+            guid: this.props.location.query.dataKey,//页面传入表单guid
+            workFlowTitle: "",
         }
 
     }
-    
+
+    componentDidMount() {
+        const {e, dataKey} = this.props.location.query;
+        CommonService.getWorkFlowTitle(e, dataKey)
+            .then(res => res.rows)
+            .then(workFlowTitle => {
+                this.setState({
+                    workFlowTitle
+                });
+            })
+            .catch(error => {
+                console.error("error", error);
+            })
+    }
+
     /*从子组件获取输入框的信息*/
     evSetRemarkTxt(val) {
-    	let th=this;
-        let allSearchArg=th.state.allSearchArg;
-        allSearchArg["remarkTxt"]=val;
+        let th = this;
+        let allSearchArg = th.state.allSearchArg;
+        allSearchArg["remarkTxt"] = val;
         th.setState({
             allSearchArg
         });
     }
 
     render() {
+        const {workFlowTitle} = this.state;
+
         let th = this;
         return <section className="ProcessApproval">
-            <ProcessApprovalTab  id={th.props.location.query["e"]} current="ProcessApprover" allSearchArg={th.state.allSearchArg}  />
-            <h3 className="boxGroupTit"><p><span>审批流程</span></p></h3>
+            <ProcessApprovalTab id={th.props.location.query["e"]} current="ProcessApprover"
+                                allSearchArg={th.state.allSearchArg}/>
+            <h3 className="boxGroupTit"><p><span>审批流程</span><span className="work-flow-title">{workFlowTitle}</span></p>
+            </h3>
             <article>
                 <div className="mgT10">
-                    <ApprovalControlNode2 guid={th.state.guid}  allSearchArg={th.state.allSearchArg} callback={th.evSetRemarkTxt.bind(th)}/>
+                    <ApprovalControlNode2 guid={th.state.guid} allSearchArg={th.state.allSearchArg}
+                                          callback={th.evSetRemarkTxt.bind(th)}/>
                 </div>
             </article>
         </section>
 
     }
 }
+
 export default ProcessApproval;
