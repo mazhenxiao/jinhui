5d186a14dc59ae503405fb37842fb61f6ad4cd89
mazhenxiao <mazhenxiao@hotmail.com>
2017-12-26 11:34:45 +0800

    测试css加载

--- components/component-newProject.js
+++ components/component-newProject.js
@@ -5,7 +5,7 @@ import NewProjectCount from "./component-newProject-count.js";
 import DynamicTable from "./tools-dynamicTable.js";
 import {Project} from '../services';
 import NewProjectTime from "./component-newProject-time.js"
-import "../css/tools-dynamicTable.less";//专用css
+require("../css/tools-dynamicTable.less");//专用css
 /*  import Peripheral from "./component-newProject-peripheral.js";//外设条件  */
 
 /*
