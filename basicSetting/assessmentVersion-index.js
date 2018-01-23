136a1c8e53171330863cae1154d14aec9733b4a3
yangmin <yangmin101225@163.com>
2018-01-23 16:10:48 +0800

    供销存

--- basicSetting/assessmentVersion-index.js
+++ basicSetting/assessmentVersion-index.js
@@ -1,10 +1,11 @@
 import "babel-polyfill";  //兼容ie
 import iss from "../js/iss.js";
 import React, {Component, Children} from 'react';
-import { Table, Icon, Divider, Select, Calendar } from 'antd';
+import { Table, Icon, Divider, Select, Calendar,Row, Col } from 'antd';
 import "./css/assessmentVersion.less";
 const Option = Select.Option;
 const { Column, ColumnGroup } = Table;
+import "../css/button.less";
 class assessmentVersionIndex extends Component {
     
         state = {
@@ -33,110 +34,42 @@ class assessmentVersionIndex extends Component {
         $(window).trigger("EVENT_CLOSELEFTBAR");
        },1000)
     }
-    selectRender () {
-        return <div>
-                    <Select defaultValue="1" style={{ width: 100 }}>
-                        <Option value="1">1</Option>
-                        <Option value="2">2</Option>
-                        <Option value="3">3</Option>
-                        <Option value="4">4</Option>
-                        <Option value="5">5</Option>
-                        <Option value="6">6</Option>
-                        <Option value="7">7</Option>
-                        <Option value="8">8</Option>
-                        <Option value="9">9</Option>
-                        <Option value="10">10</Option>
-                        <Option value="11">11</Option>
-                        <Option value="12">12</Option>
-                    </Select>
-                </div>
+
+    BIND_Save =()=>{
+
+    }
+    renderHeader = () => {
+
+            return (
+                <div>
+                    <div className="boxGroupTit">
+                        <Row>
+                            <Col span={12} >
+                                <p><span>考核版本设置</span></p>
+                            </Col>
+                            <Col span={12}>
+                                <button type="button" onClick={this.BIND_Save} className="jh_btn jh_btn22 jh_btn_add">保存</button>
+                            </Col>
+                        </Row>
+                    </div>
+                </div> 
+                
+            );
     }
 
-    tableRender () {
-    }
-    
-    timeRender () {
-        function onSelect(value) {
-            console.log(new Date(value._d).Format('yyyy-MM-dd'))
-        }
-        return <div style={{ width: 300, border: '1px solid #d9d9d9', borderRadius: 4 }}>
-                    <Calendar fullscreen={false} onSelect={onSelect} />
-                </div>
-    }
-    time_ONCLICK () {
-        var th=this;
-        iss.Alert({
-            title:"",
-            width:300,
-            height:300,
-            content:`<div>`+this.timeRender()+`</div>`,
-            okVal:"确定",
-            cancel:"取消",
-            ok(){
-                
-            },
-            cancel(da){
-                
-            }
-        })
-    }
     render() {
         return (
             <div className="">
                 <div className="version-title">
+                            {this.renderHeader()}
+
                     <h3 className="boxGroupTit">
-                        <p> 
-                            <span className='title'>考核版本设置</span>
-                        </p>
+                        
                     </h3>
                 </div>
                 <div className="version-content center">
-                     <table>
-                     <thead>
-                        <tr>
-                            <th rowSpan="2">
-                                <span>项目名称</span>
-                            </th>
-                            <th colSpan="2">
-                                <span>考核版设置</span>
-                            </th>
-                            <th rowSpan="2">
-                                <span>记尾盘日期</span>
-                            </th>
-                            <th rowSpan="2">
-                                <span>关键指标计划</span>
-                            </th>
-                        </tr>
-                        <tr>
-                            <th>
-                                <span>年初</span>
-                            </th>
-                            <th>
-                                <span>年中</span>
-                            </th>
-                        </tr>
-                    </thead>
-                        <tbody>
-                            <tr>
-                                <td></td>
-                                <td>{this.selectRender()}</td>
-                                <td>{this.selectRender()}</td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td>{this.selectRender()}</td>
-                                <td>{this.selectRender()}</td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            
-                        </tbody>
-                    </table>
-                    
+
                 </div>
-                {this.time_ONCLICK()}
             </div>
         );
     }
